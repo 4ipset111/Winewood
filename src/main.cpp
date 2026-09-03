b3622506815d09efea9c5e1b963bb88f60838db0
valmme <vagif.fb12@gmail.com>
2026-09-03 15:49:03 +0400

    copy resources to build dir

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,8 @@ int main()
 	InitWindow(1280, 720, "Winewood", RendererType::OpenGL);
     SetTargetFPS(GetMonitorRefreshRate(GetCurrentMonitor()));
 
+	Resources.Load<Texture2D>("test", "resources/test.png");
+
 	while (!WindowShouldClose())
 	{
 		BeginDrawing();
@@ -16,8 +18,11 @@ int main()
         DrawDebugText(TextFormat("%d", GetFPS()), 0, 0, 24, Color{255, 255, 255, 255});
         DrawDebugText("Winewood", 0, 24, 24, Color{255, 255, 255, 255});
 
+		auto& tex = Resources.Get<Texture2D>("test");
+		DrawTexture(tex, 0, 0, WHITE);
+		
 		EndDrawing();
-	}
+		}
 
 	CloseWindow();
 	return 0;
