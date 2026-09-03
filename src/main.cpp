2fe5017d86675063465707b18f8cb0bfcddd5f95
4ipset <228gus228uu@gmail.com>
2026-09-03 14:27:36 +0400

    base

---
+++ src/main.cpp
@@ -0,0 +1,22 @@
+#include "QuarkCore/QuarkCore.hpp"
+using namespace qc;
+
+int main()
+{
+	InitWindow(1280, 720, "Winewood", RendererType::OpenGL);
+    SetTargetFPS(GetMonitorRefreshRate(GetCurrentMonitor()));
+
+	while (!WindowShouldClose())
+	{
+		BeginDrawing();
+		ClearBackground(Color{20, 24, 32, 255});
+
+        DrawDebugText(TextFormat("%d", GetFPS()), 0, 0, 24, Color{255, 255, 255, 255});
+        DrawDebugText("Winewood", 0, 24, 24, Color{255, 255, 255, 255});
+        
+		EndDrawing();
+	}
+
+	CloseWindow();
+	return 0;
+}
